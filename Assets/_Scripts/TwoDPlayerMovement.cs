81ca7a9acd659750e7c1a3b5ad83f699e9bf7160
mbolger2 <mbolger@ltu.edu>
2023-10-18 11:29:53 -0400

    other stuff

--- Assets/_Scripts/TwoDPlayerMovement.cs
+++ Assets/_Scripts/TwoDPlayerMovement.cs
@@ -13,6 +13,22 @@ public class TwoDPlayerMovement : MonoBehaviour
     // The movement speed of this character
     public float speed;
 
+    // How far the player must be from the floor to jump
+    public float minFloorDistance;
+
+    //
+    public Vector3 raycastOriginOffset;
+
+    // 
+    public float distanceBetweenRays;
+
+
+    [SerializeField]
+    private bool physicsMovement;
+
+    [SerializeField]
+    bool raw;
+
 
     // Start is called before the first frame update
     void Start()
@@ -24,18 +40,62 @@ public class TwoDPlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (physicsMovement)
+        {
+            PhysicsMovement();
+        }
+        else
+        {
+            KinematicMovement();
+        }
+    }
+
+    void KinematicMovement()
+    {
+
+    }
+
+    void PhysicsMovement()
+    {
+        // 
+        // Ray2D floorDetection = new Ray2D(this.transform.position, -Vector2.up);
+        Debug.DrawRay(this.transform.position + raycastOriginOffset,
+            -Vector2.up * minFloorDistance, Color.red);
+
+        bool middleRay = Physics2D.Raycast(this.transform.position + raycastOriginOffset, -Vector2.up, minFloorDistance);
+        bool leftRay = Physics2D.Raycast(this.transform.position + raycastOriginOffset - Vector3.right * distanceBetweenRays, -Vector2.up, minFloorDistance);
+        bool rightRay = Physics2D.Raycast(this.transform.position + raycastOriginOffset + Vector3.right, -Vector2.up, minFloorDistance);
+
         // On the down press of space the player jumps
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump")
+            // We can cast the position to a v2 for this operation, since the funcion
+            //
+            //&& Physics2D.Raycast(this.transform.position + raycastOriginOffset, 
+            //-Vector2.up, minFloorDistance))
+            && leftRay || rightRay)
         {
             // Add force to the reggiebody upwards
             reggiebody.AddForce(Vector2.up * jumpForce);
         }
 
-        //
-        float xMov = Input.GetAxis("Horizontal");
+        // Get the input of the player (represented as (-1,1))
+        //float xMov = Input.GetAxis("Horizontal");     // Has trailing speed after button let go
+        float xMov;    // Stops as soon as not pressed
 
+        if (raw)
+        {
+            // We can add force to the right based on that, if we want an "icey" movement
+            // reggiebody.AddForce(Vector2.right * xMov * speed * Time.deltaTime);
+            xMov = Input.GetAxisRaw("Horizontal");
+        }
+        else
+        {
+            // Or we can chane the velocity directly. Notice we're not changing the
+            // velocity in y
+            //reggiebody.velocity = new Vector2 (xMov * speed, reggiebody.velocity.y);
+            xMov = Input.GetAxisRaw("Horizontal");
 
-        reggiebody.AddForce(Vector2.right * xMov * speed * Time.deltaTime);
-        
+        }
+
     }
 }
